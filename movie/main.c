d3778b44c104fc43da9c2b4735dacb24d7e94090
galileobatallaR <galileo1324@hotmail.com>
2026-03-08 20:55:49 -0600

    Update main.c

--- movie/main.c
+++ movie/main.c
@@ -1,42 +1,69 @@
 #include <stdio.h>
+#include <string.h>
 #include "pelicula.h"
 
-int main()
-{
+int main(){
 
-    printf("Prueba del TAD Pelicula\n");
+    char titulo[100];
+    char genero[100];
+    char director[100];
+    int n;
 
-    /* TODO
-       Crear una película
-    */
-    Pelicula* p = NULL;
+    printf("Titulo de la pelicula: ");
+    fgets(titulo,100,stdin);
+    titulo[strcspn(titulo,"\n")] = 0;
 
+    printf("Genero de la pelicula: ");
+    fgets(genero,100,stdin);
+    genero[strcspn(genero,"\n")] = 0;
 
-    /* TODO
-       Agregar algunos directores
-    */
+    Pelicula* p = crearPelicula(titulo,genero);
 
+    printf("Cuantos directores tiene? ");
+    scanf("%d",&n);
+    getchar();
 
-    /* TODO
-       Imprimir película
-    */
+    for(int i=0;i<n;i++){
+        printf("Director %d: ",i+1);
+        fgets(director,100,stdin);
+        director[strcspn(director,"\n")] = 0;
+        agregarDirector(p,director);
+    }
 
+    printf("\n-- Pelicula original --\n");
+    imprimir(p);
 
-    printf("\nCambiar genero\n");
+    int opcion;
 
-    /* TODO
-       Cambiar genero
-    */
+    printf("\n1 Cambiar genero\n");
+    printf("2 Agregar director\n");
+    printf("Selecciona opcion: ");
+    scanf("%d",&opcion);
+    getchar();
 
+    if(opcion == 1){
 
-    /* TODO
-       Imprimir nuevamente
-    */
+        printf("Nuevo genero: ");
+        fgets(genero,100,stdin);
+        genero[strcspn(genero,"\n")] = 0;
 
+        cambiarGenero(p,genero);
 
-    /* TODO
-       Liberar memoria
-    */
+    }else if(opcion == 2){
+
+        printf("Nuevo director: ");
+        fgets(director,100,stdin);
+        director[strcspn(director,"\n")] = 0;
+
+        agregarDirector(p,director);
+    }
+
+    printf("\n-- Pelicula modificada --\n");
+    imprimir(p);
+
+    destruir(p);
+
+    printf("\nMemoria liberada.\n");
 
     return 0;
 }
